ed13418e156da948c40143bf62237512e19c72c5
Manuel <82036608+jsfiddler@users.noreply.github.com>
2023-02-07 08:38:11 +0100

    Update childWinPDFjs.js

--- childWinPDFjs.js
+++ childWinPDFjs.js
@@ -20,11 +20,9 @@ document.body.appendChild(_canvas);
 bc=new BroadcastChannel('XP');
 
 // load the page to the canvas!
-function loadPDF(pdf,pageNumber){
+function loadPDF(pdf,pageNumber,scale){
   pdf.getPage(pageNumber).then(function(page) {
     console.log('Page loaded');
-    
-    var scale = 0.8;
     var viewport = page.getViewport({scale: scale});
 
     // Prepare canvas using PDF page dimensions
@@ -43,6 +41,7 @@ function loadPDF(pdf,pageNumber){
       console.log('Page rendered');
     });
   });
+hideDownload();
 };
 
 // Message from parent window, activates loading the PDF in ChildWindow via broadcastChannels
@@ -51,6 +50,38 @@ bc.onmessage=(evt)=>	{
 			.then(_pdf=>	{
 					console.log(_pdf);
 					pdf=_pdf;
-					loadPDF(_pdf,1);
+					loadPDF(_pdf,1,1);
 					})
 			};
+
+
+// Part 3: Add CSS
+
+const injectCSS = css => {
+  let el = document.createElement('style');
+  el.type = 'text/css';
+  el.innerText = css;
+  document.head.appendChild(el);
+  return el;
+};
+
+
+
+function hideDownload(){
+	injectCSS(`	.center-cropped {
+		  position: absolute;
+		  width: ${_canvas.width}px;
+		  height: ${_canvas.height}px;
+		  background-position: center center;
+		  background-repeat: no-repeat;
+		}
+		.overlay 	{opacity: 0.01;	}
+	`);
+	_canvas.classList.add('center-cropped');
+	_overlayer=document.createElement('div');
+	_overlayer.classList.add('overlay');
+	_overlayer.classList.add('center-cropped');
+	_overlayer.setAttribute('style','background-image: url("about:blank");')
+	_overlayer.textContent='&nbsp;';
+	document.body.appendChild(_overlayer);
+}
